077574b85aa7c6e3247224c02cc31f2ef8d6aee5
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-02-04 18:43:38 -0500

    added classes for Circle, Rectangle, and Triangle

--- index.js
+++ index.js
@@ -24,9 +24,44 @@ class Shape {
     let svg = `
       <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
         <rect width="300" height="200" fill="${this.colour}" />
-        <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="48">${this.title}</text>
+        <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="60">${this.title}</text>
       </svg>
     `;
+    return svg;
+  }
+}
+
+class Circle extends Shape {
+  render() {
+    let svg = `
+      <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="80" fill="${this.colour}" />
+        <text x="150" y="125" text-anchor="middle" fill="white" font-size="60">${this.title}</text>
+      </svg>
+    `;
+    return svg;
+  }
+}
+
+class Rectangle extends Shape {
+  render() {
+    let svg = `
+      <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="300" height="200" fill="${this.colour}" />
+        <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="60">${this.title}</text>
+      </svg>
+    `;
+    return svg;
+  }
+}
+
+class Triangle extends Shape {
+  render() {
+    let svg = `
+      <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="150,0 25,180 275,180" fill="${this.colour}" />
+        <text x="150" y="125" text-anchor="middle" fill="white" font-size="60">${this.title}</text>
+      </svg>`
     return svg;
   }
 }
